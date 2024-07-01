e09c4eacd4d2c2e5626dcb1627f071bab29c904a
Pankaj <kumarpankaj@rwit.io>
2024-07-01 18:11:57 +0530

    Added scripting project pagination

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,10 @@ var swiper = new Swiper(".projectSwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
   breakpoints: {
     640: {
       slidesPerView: 2,
